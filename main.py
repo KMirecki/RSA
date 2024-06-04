2df571ce9f19a91014e387479fa6b85e98eabb25
KMirecki <k.mirecki13@wp.pl>
2024-06-04 22:49:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -39,10 +39,11 @@ class Chunk:
         self.length_translated = len(self.data)
         self.length = self.length_translated.to_bytes(4, 'big')
         self.update_checksum()
+        self.length_translated+=12
 
     def decrypt_IDAT(self, private_key, product):
         decrypted_data = []
-        key_size = (product.bit_length() + 7) // 8  # The size of the RSA key in bytes
+        key_size = (product.bit_length() + 7) // 8 
         for i in range(0, len(self.data), key_size):
             chunk = self.data[i:i + key_size]
             encrypted_byte = int.from_bytes(chunk, 'big')
@@ -52,6 +53,46 @@ class Chunk:
         self.length_translated = len(self.data)
         self.length = self.length_translated.to_bytes(4, 'big')
         self.update_checksum()
+        self.length_translated+=12
+    
+    def encrypt_decompressed_IDAT(self, public_key, product):
+        decompressed_data = zlib.decompress(bytes(self.data))
+        block_size = 4
+        encrypted_data = []
+        for i in range(0, len(decompressed_data), block_size):
+            block = decompressed_data[i:i + block_size]
+            block_int = int.from_bytes(block, 'big')
+            encrypted_block = encrypt(block_int, public_key, product)
+            encrypted_data.extend(encrypted_block.to_bytes((product.bit_length() + 7) // 8, 'big'))
+        compressed_data = zlib.compress(bytes(encrypted_data))
+        self.data = list(compressed_data)
+        self.length_translated = len(self.data)
+        self.length = self.length_translated.to_bytes(4, 'big')
+        self.update_checksum()
+        self.length_translated+=12
+    
+    def decrypt_decompressed_IDAT(self, private_key, product):
+        decompressed_data = zlib.decompress(bytes(self.data))
+        decrypted_data = []
+        key_size = (product.bit_length() + 7) // 8
+        block_size = 4
+        for i in range(0, len(decompressed_data), key_size):
+            chunk = decompressed_data[i:i + key_size]
+            encrypted_block = int.from_bytes(chunk, 'big')
+            decrypted_block = decrypt(encrypted_block, private_key, product)
+            decrypted_data.extend(decrypted_block.to_bytes(block_size, 'big'))
+        compressed_data = zlib.compress(bytes(decrypted_data))
+        self.data = list(compressed_data)
+        self.length_translated = len(self.data)
+        self.length = self.length_translated.to_bytes(4, 'big')
+        self.update_checksum()
+        self.length_translated+=12
+
+    def encrypt_with_lib():
+        return 0
+    
+    def decrypt_with_lib():
+        return 0
 
 def save_decimal_data(png_file):
     decimal_data = []
@@ -117,51 +158,97 @@ def decrypt(encrypted_message, private_key, product):
 
 if __name__ == "__main__":
     product, public_key, private_key = generate_keys(bits=2048)
+    print(public_key, " ", private_key)
     png_file = 'pp0n6a08.png'
     dec_data = save_decimal_data(png_file)
     png_file_signature = [137, 80, 78, 71, 13, 10, 26, 10]
     chunks_list = []
-    new_chunks_list = []
-    new_chunks_list.extend(png_file_signature)
+    encrypted_data = []
+    encrypted_decompressed = []
+    encrypted_data.extend(png_file_signature)
+    encrypted_decompressed.extend(png_file_signature)
     if dec_data[0:8] == png_file_signature:
         print("Pierwsze 8 bajtow pliku - sygnatura PNG:\n", dec_data[0:8])  
         chunks_list = chunk_decoder(dec_data[8:], chunks_list)
+        chunks_list2 = chunks_list
+
+        #skompresowane
         for chunk in chunks_list:
-            print(chunk.printInfo())
             if chunk.name_translated == "IDAT":
                 if chunk.length_translated == 0:
                     break
                 else:
+                    print("oryginalny")
+                    print(chunk.printInfo())
                     chunk.encrypt_IDAT(public_key, product)
-                    new_chunks_list.extend(chunk.printBytes())
+                    encrypted_data.extend(chunk.printBytes())
+                    print("po szyfrowaniu")
                     print(chunk.printInfo())
                     chunk.decrypt_IDAT(private_key, product)
+                    #print(chunk.printBytes())
+                    print("po deszyfrowaniu")
                     print(chunk.printInfo())
             else:
-                new_chunks_list.extend(chunk.printBytes())
+                encrypted_data.extend(chunk.printBytes())
           
-        data_bytes = []
-        data_bytes.extend(png_file_signature)
+        decrypted_data = []
+        decrypted_data.extend(png_file_signature)
         for chunk in chunks_list:
-            data_bytes.extend(chunk.printBytes())
+            decrypted_data.extend(chunk.printBytes())
 
         with open("decrypted.png","wb") as f:
-            f.write(bytes(data_bytes))
+            f.write(bytes(decrypted_data))
         
         with open("encrypted.png","wb") as f:
-            f.write(bytes(new_chunks_list))
-
-        image = Image.open(io.BytesIO(bytes(data_bytes)))
+            f.write(bytes(encrypted_data))
+        
+        image = Image.open(io.BytesIO(bytes(decrypted_data)))
         image1 = Image.open("decrypted.png")
 
+        #zdekompresowane
+        for chunk in chunks_list2:
+            if chunk.name_translated == "IDAT":
+                if chunk.length_translated == 0:
+                    break
+                else:
+                    print("oryginalny")
+                    print(chunk.printInfo())
+                    chunk.encrypt_decompressed_IDAT(public_key, product)
+                    encrypted_decompressed.extend(chunk.printBytes())
+                    print("po szyfrowaniu po dekompresji")
+                    print(chunk.printInfo())
+                    chunk.decrypt_decompressed_IDAT(private_key, product)
+                    #print(chunk.printBytes())
+                    print("po deszyfrowaniu po kompresji")
+                    print(chunk.printInfo())
+            else:
+                encrypted_decompressed.extend(chunk.printBytes())
+
+        decrypted_data2 = []
+        decrypted_data2.extend(png_file_signature)
+        for chunk in chunks_list2:
+            decrypted_data2.extend(chunk.printBytes())
+
+        with open("encrypted_decompressed.png","wb") as f:
+            f.write(bytes(encrypted_decompressed))
+
+        with open("decrypted_decompressed.png","wb") as f:
+            f.write(bytes(decrypted_data2))
+
+        image2 = Image.open("decrypted_decompressed.png")
+
         plt.figure()
-        plt.subplot(1, 2, 1)
+        plt.subplot(1, 3, 1)
         plt.imshow(image)
         plt.title('original image')
         plt.axis('off')
-        plt.subplot(1, 2, 2)
+        plt.subplot(1, 3, 2)
         plt.imshow(image1)
         plt.title('decrypted image')
+        plt.axis('off')
+        plt.subplot(1, 3, 3)
+        plt.imshow(image2)
+        plt.title('decompressed decrypted image')
         plt.axis('off')
         plt.show()
 
