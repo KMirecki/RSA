6e19f0dfed82f5c9cde241b9c3e02ca84a8e9098
KMirecki <k.mirecki13@wp.pl>
2024-06-04 14:17:38 +0200

    działające rsa bez dekompresji

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import zlib
 
 class Chunk:
     def __init__(self, length, name, length_translated, name_translated, data, checksum):
-        #wartości liczbowe/tekstowe zamiast listy bajtów
         self.length_translated = length_translated
         self.name_translated = name_translated
         self.length = length
@@ -19,7 +18,6 @@ class Chunk:
     def printInfo(self):
         return "\nDługość chunku:\t" + str(self.length_translated) + "\nNazwa chunku:\t" + str(self.name_translated)
     
-    #wypisanie wszystkich bajtów chunka
     def printBytes(self):
         bytes_list = []
         bytes_list.extend(self.length)
@@ -35,25 +33,32 @@ class Chunk:
     def encrypt_IDAT(self, public_key, product):
         encrypted_data = []
         for byte in self.data:
-            encrypted_byte = encrypt(byte,public_key,product)
-            encrypted_data.append(encrypted_byte)
+            encrypted_byte = encrypt(byte, public_key, product)
+            encrypted_data.extend(encrypted_byte.to_bytes((product.bit_length() + 7) // 8, 'big'))
         self.data = encrypted_data
+        self.length_translated = len(self.data)
+        self.length = self.length_translated.to_bytes(4, 'big')
+        self.update_checksum()
 
     def decrypt_IDAT(self, private_key, product):
         decrypted_data = []
-        for byte in self.data:
-            decrypted_byte = decrypt(byte,private_key,product)
+        key_size = (product.bit_length() + 7) // 8  # The size of the RSA key in bytes
+        for i in range(0, len(self.data), key_size):
+            chunk = self.data[i:i + key_size]
+            encrypted_byte = int.from_bytes(chunk, 'big')
+            decrypted_byte = decrypt(encrypted_byte, private_key, product)
             decrypted_data.append(decrypted_byte)
         self.data = decrypted_data
+        self.length_translated = len(self.data)
+        self.length = self.length_translated.to_bytes(4, 'big')
+        self.update_checksum()
 
 def save_decimal_data(png_file):
     decimal_data = []
     with open(png_file, 'rb') as f:
         binary_data = f.read()
-
         for byte in binary_data:
             decimal_data.append(byte)
-
     png_check = decimal_data[0:8]
     if png_check == [137, 80, 78, 71, 13, 10, 26, 10]:
         return decimal_data
@@ -71,16 +76,16 @@ def chunk_length(chunk):
     return chunk_length
 
 def chunk_decoder(chunk, chunks_list):
-    if(len(chunk)>=12):
+    if len(chunk) >= 12:
         length = chunk[0:4]
         length_translated = chunk_length(chunk[0:4])
         name = chunk[4:8]
         name_translated = chunk_name(chunk[4:8])
         data = chunk[8:length_translated-4]
         checksum = chunk[length_translated-4:length_translated]
-        new_chunk = Chunk(length,name,length_translated,name_translated,data,checksum)
+        new_chunk = Chunk(length, name, length_translated, name_translated, data, checksum)
         chunks_list.append(new_chunk)
-        remaining_chunk=chunk[length_translated:]
+        remaining_chunk = chunk[length_translated:]
         chunk_decoder(remaining_chunk, chunks_list)
     return chunks_list
 
@@ -91,18 +96,17 @@ def generate_public_key(totient):
             return public_key
 
 def generate_private_key(public_key, totient):
-    return pow(public_key,-1,totient)
+    return pow(public_key, -1, totient)
 
-def generate_keys():
-    p = sympy.randprime(2, 15)
-    q = sympy.randprime(2, 15)
+def generate_keys(bits=2048):
+    p = sympy.randprime(pow(2, (bits // 2 - 1)), pow(2, (bits // 2)))
+    q = sympy.randprime(pow(2, (bits // 2 - 1)), pow(2, (bits // 2)))
     while p == q:
-        q = sympy.randprime(2, 15)
+        q = sympy.randprime(pow(2, (bits // 2 - 1)), pow(2, (bits // 2)))    
     product = p * q
     totient = (p - 1) * (q - 1)
     public_key = generate_public_key(totient)
     private_key = generate_private_key(public_key, totient)
-    #print(f"p = {p}, q = {q}, product = {product}, totient = {totient}, public key = {public_key}, private key = {private_key}")
     return product, public_key, private_key
 
 def encrypt(message, public_key, product):
@@ -112,67 +116,54 @@ def decrypt(encrypted_message, private_key, product):
     return pow(encrypted_message, private_key, product)
 
 if __name__ == "__main__":
-    #message = 60
-    product, public_key, private_key = generate_keys()
-    #encrypted_message = encrypt(message, public_key, product)
-    #print(f"Encrypted message: {encrypted_message}")
-    #decrypted_message = decrypt(encrypted_message, private_key, product)
-    #print(f"Decrypted message: {decrypted_message}")
-    #png_file = 'Lenna_(test_image).png'
-    #png_file = 'PNG_transparency_demonstration_1.png'
-    #png_file = 'pnglogo--povray-3.7--black826--800x600.png'
+    product, public_key, private_key = generate_keys(bits=2048)
     png_file = 'pp0n6a08.png'
     dec_data = save_decimal_data(png_file)
     png_file_signature = [137, 80, 78, 71, 13, 10, 26, 10]
-    chunks_list=[]
-    if(dec_data[0:8]==png_file_signature):
+    chunks_list = []
+    new_chunks_list = []
+    new_chunks_list.extend(png_file_signature)
+    if dec_data[0:8] == png_file_signature:
         print("Pierwsze 8 bajtow pliku - sygnatura PNG:\n", dec_data[0:8])  
-        chunks_list = chunk_decoder(dec_data[8:],chunks_list)
+        chunks_list = chunk_decoder(dec_data[8:], chunks_list)
         for chunk in chunks_list:
             print(chunk.printInfo())
-            match chunk.name_translated:
-                case "IDAT":
-                    if(chunk.length_translated==0):
-                        break
-                    else:
-                        chunk.encrypt_IDAT(public_key, product)
-                        chunk.update_checksum()
-                        chunk.decrypt_IDAT(private_key,product)
-                        chunk.update_checksum()
-                
-        data_bytes = bytes(dec_data)
-        image = Image.open(io.BytesIO(data_bytes))
+            if chunk.name_translated == "IDAT":
+                if chunk.length_translated == 0:
+                    break
+                else:
+                    chunk.encrypt_IDAT(public_key, product)
+                    new_chunks_list.extend(chunk.printBytes())
+                    print(chunk.printInfo())
+                    chunk.decrypt_IDAT(private_key, product)
+                    print(chunk.printInfo())
+            else:
+                new_chunks_list.extend(chunk.printBytes())
+          
+        data_bytes = []
+        data_bytes.extend(png_file_signature)
+        for chunk in chunks_list:
+            data_bytes.extend(chunk.printBytes())
 
-        #print(newer_chunks_list)
-        #with open("plik.png","wb") as f:
-        #    f.write(bytes(newer_chunks_list))
+        with open("decrypted.png","wb") as f:
+            f.write(bytes(data_bytes))
         
-        #print(new_chunks_list)
-        #with open("plik1.png","wb") as f:
-        #    f.write(bytes(new_chunks_list))
+        with open("encrypted.png","wb") as f:
+            f.write(bytes(new_chunks_list))
 
-        #new_data_bytes = bytes(new_chunks_list)
-        #image1 = Image.open(io.BytesIO(new_data_bytes))
-
-        #newer_data_bytes = bytes(newer_chunks_list)
-        #image1 = Image.open(io.BytesIO(newer_data_bytes))
-
-        #with open("lena.png", "wb") as f:
-        #    f.write(bytes(newer_chunks_list))
-        
-        #with open("lena1.png", "wb") as f:
-        #    f.write(bytes(newer_chunks_list))
+        image = Image.open(io.BytesIO(bytes(data_bytes)))
+        image1 = Image.open("decrypted.png")
 
         plt.figure()
         plt.subplot(1, 2, 1)
         plt.imshow(image)
-        plt.title('Image')
+        plt.title('original image')
         plt.axis('off')
         plt.subplot(1, 2, 2)
         plt.imshow(image1)
-        plt.title('Image')
+        plt.title('decrypted image')
         plt.axis('off')
         plt.show()
 
     else:
-        raise TypeError("File is not a png")
+        raise TypeError("File is not a PNG")
